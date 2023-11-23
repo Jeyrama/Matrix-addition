86b11e76f7d9f40b5e9b876a313296e7a0fb5c27
Jey <126204426+Jeyrama@users.noreply.github.com>
2023-11-23 15:22:14 -0800

    Update Matrix-addition.js

--- Matrix-addition.js
+++ Matrix-addition.js
@@ -31,3 +31,14 @@ Example:
 
 
 // Solution
+
+function matrixAddition(a, b) {
+  let c = [];
+  for(let n = 0; n < a.length; n++) {
+    c[n] = [];
+    for(let m = 0; m < a[n].length; m++) {
+      c[n][m] = a[n][m] + b[n][m];
+    }
+  }
+  return c;
+}
